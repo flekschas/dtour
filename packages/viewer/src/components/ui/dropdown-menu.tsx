eba057ca0af8fa5fe859c8f41c8fe289f8efefa7
Fritz Lekschas <code@lekschas.de>
2026-05-03 16:10:09 -0400

    chore: make hover bg color more subtle

--- packages/viewer/src/components/ui/dropdown-menu.tsx
+++ packages/viewer/src/components/ui/dropdown-menu.tsx
@@ -33,7 +33,7 @@ export const DropdownMenuItem = ({
 }: ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Item>) => (
   <DropdownMenuPrimitive.Item
     className={cn(
-      'relative flex cursor-pointer select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none transition-colors focus:bg-dtour-border focus:text-dtour-highlight data-[disabled]:pointer-events-none data-[disabled]:opacity-50',
+      'relative flex cursor-pointer select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none transition-colors focus:bg-dtour-surface data-highlighted:bg-dtour-surface focus:text-dtour-highlight data-disabled:pointer-events-none data-disabled:opacity-50',
       className,
     )}
     {...props}
